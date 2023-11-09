c6621449528cbe442ba4a40a014f90881378e428
Gowtham MA <95484326+Magowtham@users.noreply.github.com>
2023-11-10 00:03:19 +0530

    Update delete_bird.js

--- Server/Controllers/AdminPanel/delete_bird.js
+++ Server/Controllers/AdminPanel/delete_bird.js
@@ -2,9 +2,9 @@ const BirdModel = require("../../Models/bird_data");
 const mongoose = require("mongoose");
 const deleteBird = async (req, res) => {
   try {
-    const { id } = req.body;
-    const _id = new mongoose.Types.ObjectId(id);
-    const deleteResult = await BirdModel.deleteOne({ _id });
+    let { id } = req.body;
+     id = new mongoose.Types.ObjectId(id);
+    const deleteResult = await BirdModel.deleteOne({ _id: id});
     console.log(deleteResult);
     res.status(200).json({ message: "bird deleted successfully" });
   } catch (error) {
